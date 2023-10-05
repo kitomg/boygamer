5719720e1a4a273af43fe662bf3b48716de19546
Gustavo Cha <gusjavierc123@gmail.com>
2023-10-05 18:18:31 -0300

    Agregado primer php

---
+++ desktop/procesar_login.php
@@ -0,0 +1,14 @@
+<?php
+
+if (empty($_POST['usuario']) || empty($_POST['contrasena'])) {
+    $redirigir = 'proyectofinal.php?mensaje=Error: Falta un campo obligatorio';
+} else {
+    $cs = new ControladorInicio();
+    $login = $cs->login($_POST['usuario'], $_POST['contrasena']);
+    if ($login[0] === true) {
+        $redirigir = 'principal.php';
+    } else {
+        $redirigir = 'proyectofinal.html?mensaje=' . $login[1];
+    }
+}
+header('Location: ' . $redirigir);
