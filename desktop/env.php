c4386fef8a1961111d3d2e9215f1cbae9a95122e
Nicolas Pellicciotti <nicolas.pellicciotti@gmail.com>
2023-10-19 00:54:49 -0300

    credenciales

---
+++ desktop/env.php
@@ -0,0 +1,11 @@
+<?php
+function credenciales()
+{
+    return
+        [
+            'usuario' => 'root',
+            'contrasena' => 'root',
+            'servidor' => '127.0.0.1',
+            'base_de_datos' => 'videojuegos',
+        ];
+}
